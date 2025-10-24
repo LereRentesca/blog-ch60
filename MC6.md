0a03c0f5f307d7b4dab1dbb45c0702639645579a
LereRentesca <lere.rentesca@gmail.com>
2025-10-23 20:10:04 -0700

    Class #2 - FSDI113

---
+++ MC6.md
@@ -0,0 +1,3 @@
+# Mini Challenge 6 - PostListView and PostDetailView
+
+Create two views inside of the post applications to display all of the posts and also a detailed view for each one if the user clicks on it from the list page.
--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailedView
+)
 
 urlpatterns = [
-    
+    path("", PostListView.as_view(), name="post_list"),
+    path("detailed/<int:pk>/", PostDetailedView.as_view(), name="post_detail"),
 ]
